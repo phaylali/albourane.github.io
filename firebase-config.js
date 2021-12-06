f84b0e79c0c108fdce1fa65f13e1cbdf29c2d215
Ayoub El Failali <elfailaliayoub@gmail.com>
2021-12-06 17:23:52 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+export var firebaseConfig = {
+    apiKey: "AIzaSyAWPjVezSEy3WaSZXwF8PmySHfo2CCHnQQ",
+    authDomain: "albourane.firebaseapp.com",
+    databaseURL: "https://albourane.firebaseio.com",
+    projectId: "albourane",
+    storageBucket: "albourane.appspot.com",
+    messagingSenderId: "636844806568",
+    appId: "1:636844806568:web:ef8fc5a443aa4b390f4d9d",
+    measurementId: "G-L2CWMM1XC4"
+  };
